37e3365d92d1999aaf4f4bc1977d8a1d028f09dc
Екатерина <ekaterina.s3006@yandex.ru>
2023-11-26 19:47:41 +0300

    add filters and basic search for our models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
+from django.db.models import Q
+
 from .models import Category, Product
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "price", "category"]
+    list_filter = ["category"]
+    search_fields = ["name", "description"]
 
-admin.site.register(Category)
-admin.site.register(Product)
+    def get_search_results(self, request, queryset, search_term):
+        search_term_list = search_term.split(' ')
+
+        if not any(search_term_list):
+            return queryset, False
+
+        name_query = Q(name__icontains=search_term)
+        description_query = Q(description__icontains=search_term)
+        queryset = queryset.filter(name_query | description_query)
+
+        return queryset, False
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
