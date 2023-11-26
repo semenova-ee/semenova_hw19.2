c322469f3915781b8e502b651dfca1e53e3828db
Екатерина <ekaterina.s3006@yandex.ru>
2023-11-26 19:09:23 +0300

    homework 20.1 create database

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hw20.1',
+        'USER': 'postgres',
+        'PASSWORD': 'Katerina321',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
