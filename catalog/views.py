c609855b9eaa59b76ea6a08c924e3f1c0b1d4be7
Екатерина <ekaterina.s3006@yandex.ru>
2023-12-03 17:06:25 +0300

    homework 20.2 add base.html, head.html add controller for displaing products from database

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def index(request):
-    return render(request, "catalog/index.html")
+    products = Product.objects.all()
+    return render(request, "catalog/index.html", {'products': products})
 
 def about(request):
     return render(request, "catalog/about.html")
-
